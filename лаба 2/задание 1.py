da2da3dcb9fcc74b91ec8eed8ac6e3b4f1b056b4
NastyaIvan <borisova.ai@edu.spbstu.ru>
2026-03-17 16:27:51 +0300

    Update задание 1.py

--- лаба 2/задание 1.py
+++ лаба 2/задание 1.py
@@ -6,9 +6,12 @@ increase = 0.05  # Ежемесячный рост цен
 all_money = money_capital + salary
 month = 0
 expenses = spend
-while all_money >= expenses:
+while all_money >= expenses: 
+    #  пока денег хватает на жизнь
     month += 1
     all_money = all_money - expenses + salary
+    # вычитаем траты из общего бюджета
     expenses *= 1 + increase
+    # увеличение трат каждыйц месяц
 
 print("Количество месяцев, которое можно протянуть без долгов:", month)
