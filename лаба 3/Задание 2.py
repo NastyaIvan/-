8ad7e32b71074e602f8b4fca2642142e64b6e3cd
NastyaIvan <borisova.ai@edu.spbstu.ru>
2026-03-22 19:12:21 +0300

    Update Задание 2.py

--- лаба 3/Задание 2.py
+++ лаба 3/Задание 2.py
@@ -6,12 +6,8 @@ def find_common_participants(company_1,company_2, r = ','):
     # приводим обе строки к множеству, выбираем общее и сортируем
     return same_list
 
-
-
-
 participants_first_group = "Иванов|Петров|Сидоров"
 participants_second_group = "Петров|Сидоров|Смирнов"
 
 # TODO Провеьте работу функции с разделителем отличным от запятой
-
 print(find_common_participants(participants_first_group, participants_second_group, '|'))
